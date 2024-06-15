001f43b976dd7757fa94267b80fed3d3d529a644
fasnap <fasnapkdy@gmail.com>
2024-06-16 01:39:10 +0530

    sub domain

--- glammart/settings.py
+++ glammart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['18.209.24.30']
+ALLOWED_HOSTS = ['18.209.24.30','127.0.0.1', 'fazglam.strangled.net']
 
 
 # Application definition
